fce4f25f5fba1c5c0eaa7b5911c9135143858d6a
jalda123 <sateesh.jalda2020@gmail.com>
2018-07-02 12:30:38 +0530

    first commit

---
+++ src/test/java/com/Naveen/JenkinsDemo/JenkinsGit.java
@@ -0,0 +1,13 @@
+package com.Naveen.JenkinsDemo;
+
+import org.testng.annotations.Test;
+
+public class JenkinsGit {
+	
+	@Test
+	
+	public void demo() {
+		System.out.println("Hello jenkins");
+	}
+
+}
